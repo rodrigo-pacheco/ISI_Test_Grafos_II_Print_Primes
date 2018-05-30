5f406ff25d1268f95d77682bc1d0bf0bbd016d32
Rodrigo_P <r.pachecom@alumnos.urjc.es>
2018-05-30 09:07:30 +0200

    Creo el fichero de test

---
+++ isi.travis/src/test/java/org/urjc/isi/travis/PrintPrimesTest.java
@@ -0,0 +1,14 @@
+package org.urjc.isi.travis;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PrintPrimesTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
