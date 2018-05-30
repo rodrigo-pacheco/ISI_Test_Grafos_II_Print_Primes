4f9e3a09b0fb122859fa2c2ccb973bcb15f5ac58
Rodrigo_P <r.pachecom@alumnos.urjc.es>
2018-05-30 09:11:56 +0200

    creado @Before -> valores de las variables antes de los tests

--- isi.travis/src/test/java/org/urjc/isi/travis/PrintPrimesTest.java
+++ isi.travis/src/test/java/org/urjc/isi/travis/PrintPrimesTest.java
@@ -2,10 +2,20 @@ package org.urjc.isi.travis;
 
 import static org.junit.Assert.*;
 
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class PrintPrimesTest {
-
+	
+	
+	@Before
+	public void setUp() throws Exception{
+		System.setOut(new PrintStream(outContent));
+	}
+	
 	@Test
 	public void test() {
 		fail("Not yet implemented");
