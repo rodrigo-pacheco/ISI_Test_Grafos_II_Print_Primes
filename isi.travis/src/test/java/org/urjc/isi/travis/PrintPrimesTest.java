549196d27e67adb619c356a96ca0060a95b7cc77
Rodrigo_P <r.pachecom@alumnos.urjc.es>
2018-05-30 09:19:18 +0200

    Test Ejercicio 3

--- isi.travis/src/test/java/org/urjc/isi/travis/PrintPrimesTest.java
+++ isi.travis/src/test/java/org/urjc/isi/travis/PrintPrimesTest.java
@@ -2,6 +2,7 @@ package org.urjc.isi.travis;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
 
@@ -10,15 +11,21 @@ import org.junit.Test;
 
 public class PrintPrimesTest {
 	
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 	
 	@Before
 	public void setUp() throws Exception{
 		System.setOut(new PrintStream(outContent));
 	}
 	
+	// Test Ejercicio 3: edge que va del while al for (2-11) sin ejecutar el while
+	// Para eso se tiene que ejecutar PrinPrimes con un 1, ya que while (numPrimes < n) y
+	// nunPrimes = 1
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testEjercicio3() {
+		PrintPrimes.main(new String[] {"1"});
+		assertEquals("Prime: 2\n", outContent.toString());
 	}
 
 }
